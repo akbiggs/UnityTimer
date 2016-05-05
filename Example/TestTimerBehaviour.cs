5d9fb096add7b76a22713acb446ab4532d2504e6
Alexander Biggs <alexanderkbiggs@gmail.com>
2016-05-05 13:31:21 -0400

    Move the example into a separate namespace.

--- Example/TestTimerBehaviour.cs
+++ Example/TestTimerBehaviour.cs
@@ -3,128 +3,129 @@ using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 
-public class TestTimerBehaviour : MonoBehaviour
+namespace UnityTimer.Examples
 {
-    #region Unity Inspector Fields
-
-    [Header("Controls")]
-    public InputField DurationField;
-
-    public Button StartTimerButton;
-    public Button CancelTimerButton;
-    public Button PauseTimerButton;
-    public Button ResumeTimerButton;
-
-    public Toggle IsLoopedToggle;
-    public Toggle UseGameTimeToggle;
-
-    public Slider TimescaleSlider;
-
-    public Text NeedsRestartText;
-
-    [Header("Stats Texts")]
-    public Text TimeElapsedText;
-    public Text TimeRemainingText;
-    public Text PercentageCompletedText;
-    public Text PercentageRemainingText;
-
-    public Text NumberOfLoopsText;
-    public Text IsCancelledText;
-    public Text IsCompletedText;
-    public Text IsPausedText;
-    public Text IsDoneText;
-    public Text UpdateText;
-
-    #endregion
-
-    private int _numLoops;
-    private Timer _testTimer;
-
-    private void Awake()
+    public class TestTimerBehaviour : MonoBehaviour
     {
-        ResetState();
-    }
+        #region Unity Inspector Fields
 
-    private void ResetState()
-    {
-        _numLoops = 0;
-        CancelTestTimer();
-    }
+        [Header("Controls")] public InputField DurationField;
 
-    public void StartTestTimer()
-    {
-        ResetState();
+        public Button StartTimerButton;
+        public Button CancelTimerButton;
+        public Button PauseTimerButton;
+        public Button ResumeTimerButton;
 
-        // this is the important code example bit where we register a new timer
-        _testTimer = Timer.Register(
-            duration: GetDurationValue(),
-            onComplete: () => _numLoops++,
-            onUpdate: secondsElapsed =>
-            {
-                UpdateText.text = string.Format("Timer ran update callback: {0:F2} seconds", secondsElapsed);
-            },
-            isLooped: IsLoopedToggle.isOn,
-            useRealTime: !UseGameTimeToggle.isOn);
+        public Toggle IsLoopedToggle;
+        public Toggle UseGameTimeToggle;
 
-        CancelTimerButton.interactable = true;
-    }
+        public Slider TimescaleSlider;
 
-    public void CancelTestTimer()
-    {
-        Timer.Cancel(_testTimer);
-        CancelTimerButton.interactable = false;
-        NeedsRestartText.gameObject.SetActive(false);
-    }
+        public Text NeedsRestartText;
 
-    public void PauseTestTimer()
-    {
-        Timer.Pause(_testTimer);
-    }
+        [Header("Stats Texts")] public Text TimeElapsedText;
+        public Text TimeRemainingText;
+        public Text PercentageCompletedText;
+        public Text PercentageRemainingText;
 
-    public void ResumeTestTimer()
-    {
-        Timer.Resume(_testTimer);
-    }
+        public Text NumberOfLoopsText;
+        public Text IsCancelledText;
+        public Text IsCompletedText;
+        public Text IsPausedText;
+        public Text IsDoneText;
+        public Text UpdateText;
 
-    private void Update()
-    {
-        if (_testTimer == null)
+        #endregion
+
+        private int _numLoops;
+        private Timer _testTimer;
+
+        private void Awake()
         {
-            return;
+            ResetState();
         }
 
-        Time.timeScale = TimescaleSlider.value;
-        _testTimer.isLooped = IsLoopedToggle.isOn;
+        private void ResetState()
+        {
+            _numLoops = 0;
+            CancelTestTimer();
+        }
 
-        TimeElapsedText.text = string.Format("Time elapsed: {0:F2} seconds", _testTimer.GetTimeElapsed());
-        TimeRemainingText.text = string.Format("Time remaining: {0:F2} seconds", _testTimer.GetTimeRemaining());
-        PercentageCompletedText.text = string.Format("Percentage completed: {0:F4}%",
-            _testTimer.GetRatioComplete() * 100);
-        PercentageRemainingText.text = String.Format("Percentage remaining: {0:F4}%",
-            _testTimer.GetRatioRemaining() * 100);
-        NumberOfLoopsText.text = string.Format("# Loops: {0}", _numLoops);
-        IsCancelledText.text = string.Format("Is Cancelled: {0}", _testTimer.isCancelled);
-        IsCompletedText.text = string.Format("Is Completed: {0}", _testTimer.isCompleted);
-        IsPausedText.text = String.Format("Is Paused: {0}", _testTimer.isPaused);
-        IsDoneText.text = string.Format("Is Done: {0}", _testTimer.isDone);
+        public void StartTestTimer()
+        {
+            ResetState();
 
-        PauseTimerButton.interactable = !_testTimer.isPaused;
-        ResumeTimerButton.interactable = _testTimer.isPaused;
+            // this is the important code example bit where we register a new timer
+            _testTimer = Timer.Register(
+                duration: GetDurationValue(),
+                onComplete: () => _numLoops++,
+                onUpdate: secondsElapsed =>
+                {
+                    UpdateText.text = string.Format("Timer ran update callback: {0:F2} seconds", secondsElapsed);
+                },
+                isLooped: IsLoopedToggle.isOn,
+                useRealTime: !UseGameTimeToggle.isOn);
 
-        NeedsRestartText.gameObject.SetActive(ShouldShowRestartText());
-    }
+            CancelTimerButton.interactable = true;
+        }
 
-    private bool ShouldShowRestartText()
-    {
-        return !_testTimer.isDone && // the timer is in progress and...
-               ((UseGameTimeToggle.isOn && _testTimer.usesRealTime) || // we switched to real-time or
-                (!UseGameTimeToggle.isOn && !_testTimer.usesRealTime) || // we switched to game-time or
-                Mathf.Abs(GetDurationValue() - _testTimer.duration) >= Mathf.Epsilon); // our duration changed
-    }
+        public void CancelTestTimer()
+        {
+            Timer.Cancel(_testTimer);
+            CancelTimerButton.interactable = false;
+            NeedsRestartText.gameObject.SetActive(false);
+        }
 
-    private float GetDurationValue()
-    {
-        float duration;
-        return float.TryParse(DurationField.text, out duration) ? duration : 0;
+        public void PauseTestTimer()
+        {
+            Timer.Pause(_testTimer);
+        }
+
+        public void ResumeTestTimer()
+        {
+            Timer.Resume(_testTimer);
+        }
+
+        private void Update()
+        {
+            if (_testTimer == null)
+            {
+                return;
+            }
+
+            Time.timeScale = TimescaleSlider.value;
+            _testTimer.isLooped = IsLoopedToggle.isOn;
+
+            TimeElapsedText.text = string.Format("Time elapsed: {0:F2} seconds", _testTimer.GetTimeElapsed());
+            TimeRemainingText.text = string.Format("Time remaining: {0:F2} seconds", _testTimer.GetTimeRemaining());
+            PercentageCompletedText.text = string.Format("Percentage completed: {0:F4}%",
+                _testTimer.GetRatioComplete()*100);
+            PercentageRemainingText.text = String.Format("Percentage remaining: {0:F4}%",
+                _testTimer.GetRatioRemaining()*100);
+            NumberOfLoopsText.text = string.Format("# Loops: {0}", _numLoops);
+            IsCancelledText.text = string.Format("Is Cancelled: {0}", _testTimer.isCancelled);
+            IsCompletedText.text = string.Format("Is Completed: {0}", _testTimer.isCompleted);
+            IsPausedText.text = String.Format("Is Paused: {0}", _testTimer.isPaused);
+            IsDoneText.text = string.Format("Is Done: {0}", _testTimer.isDone);
+
+            PauseTimerButton.interactable = !_testTimer.isPaused;
+            ResumeTimerButton.interactable = _testTimer.isPaused;
+
+            NeedsRestartText.gameObject.SetActive(ShouldShowRestartText());
+        }
+
+        private bool ShouldShowRestartText()
+        {
+            return !_testTimer.isDone && // the timer is in progress and...
+                   ((UseGameTimeToggle.isOn && _testTimer.usesRealTime) || // we switched to real-time or
+                    (!UseGameTimeToggle.isOn && !_testTimer.usesRealTime) || // we switched to game-time or
+                    Mathf.Abs(GetDurationValue() - _testTimer.duration) >= Mathf.Epsilon); // our duration changed
+        }
+
+        private float GetDurationValue()
+        {
+            float duration;
+            return float.TryParse(DurationField.text, out duration) ? duration : 0;
+        }
     }
 }
